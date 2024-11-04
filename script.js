54f02f14503d46c24da92a3b88c839ab8e40d355
ShahzadAli88 <shahzadbudaun@gmail.com>
2024-11-04 20:41:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// ---Wrapper ---------
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const lbtn = document.querySelector('.login-btn');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=>{
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=>{
+  wrapper.classList.add('active-popup');
+});
+
+lbtn.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+  });
+
+iconClose.addEventListener('click', ()=>{
+  wrapper.classList.remove('active-popup');
+}); 
+
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    // setting owl carousel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $('#hero-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        nav:true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    })
+
+    $('#top-movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            500: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+
+    $('.movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        nav:true,
+        navText: navText,
+        margin: 15,
+        responsive: {
+            500: {
+                items: 2
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+})
